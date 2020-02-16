4c14914bc0063850dd155ee2e83b0b44632af53f
Varun <varunasthana92@gmail.com>
2020-02-16 16:58:02 -0500

    [TensorDLT] Adding TDLT.py

---
+++ Code/TDLT.py
@@ -0,0 +1,21 @@
+import glob
+import cv2
+import numpy as np 
+import random
+
+def TDLT(ca, cb, h4pt):
+	A = []
+	B = []
+	for i in range(4):
+		x, y = ca[i][0], ca[i][1]
+		u, v = cb[i][0], cb[i][1]
+		B.append(-v)
+		B.append(u)
+		tempA1 = [0 0 0 -x -y -1 x*v y*v]
+		tempA2 = [x y 1 0 0 0 -x*u -y*u]
+		A.append(tempA1)
+		A.append(tempA2)
+
+	Ainv = np.linalg.pinv(np.array(A))
+	H = np.matmul(Ainv, np.array(B))
+	return H
